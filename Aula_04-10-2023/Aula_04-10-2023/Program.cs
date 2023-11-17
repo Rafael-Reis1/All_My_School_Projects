72ac0ce9d3aca452b21ad2101703771c1409c9dc
Rafael R <146379316+Rafa3lR@users.noreply.github.com>
2023-11-17 02:37:36 -0300

    Add files via upload

---
+++ Aula_04-10-2023/Aula_04-10-2023/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sorte, chance;
+        Random numAleatorio = new Random();
+        int numero = numAleatorio.Next(1, 100);
+
+        Console.WriteLine("Jogo da sorte\n");
+        for (chance = 1; chance <= 4; chance++)
+        {
+            Console.WriteLine("Tenta a sorte (4 tentativas): ");
+            sorte = int.Parse(Console.ReadLine());  
+
+            if (numero == sorte)
+            {
+                Console.WriteLine("Você acertou !!!");
+                chance = 5;
+            }
+            else if (numero < sorte)
+            {
+                Console.Clear();
+                Console.WriteLine("O número é menor!" + "  " + chance + " Tentativas");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("O número é maior!" + "  " + chance + " Tentativas");
+            }
+        }
+        Console.WriteLine("O número é " + numero);
+    }
+}
