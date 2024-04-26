38f92152a16fe24d12728324b1dbed56cd090741
Rafael-Reis <146379316+Rafa3lR@users.noreply.github.com>
2024-04-26 03:04:48 -0300

    Performance updates and exit cadbook with esc

---
+++ Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/README.md
@@ -0,0 +1,47 @@
+## Gerenciamento de Livros em Biblioteca: Um Software Eficiente com Windows Forms e C#
+
+### Introdução
+
+Este repositório apresenta o software de gerenciamento de livros em biblioteca desenvolvido para a disciplina de Estrutura de Dados, ministrada pelo Professor Vitor Ribeiro. O sistema utiliza a linguagem de programação C# e a interface gráfica Windows Forms para oferecer uma ferramenta intuitiva e eficiente para cadastro, consulta, exclusão e busca de livros.
+
+### Funcionalidades
+
+* **Interface amigável:** Windows Forms proporciona uma interface gráfica amigável e de fácil utilização.
+* **Gerenciamento completo de livros:**
+    * Cadastro de novos livros com informações completas.
+    * Consulta de livros por diversos critérios.
+* **Busca eficiente com árvores de busca:**
+    * Implementação de Árvores Binárias de Busca (ABB) e Árvores AVL.
+    * Comparação de tempo de execução em adição, exclusão e pesquisa.
+* **Armazenamento de dados:**
+    * Salvamento em arquivos de texto (.txt) dentro do diretório do projeto.
+
+### Benefícios
+
+* **Otimização do tempo:** Agiliza o processo de gerenciamento de livros.
+* **Precisão e confiabilidade:** Busca precisa e eficiente com árvores de busca.
+* **Interface intuitiva:** Fácil utilização para diversos públicos.
+* **Comparação de desempenho:** Análise aprofundada entre ABB e AVL.
+* **Armazenamento seguro:** Segurança e persistência das informações.
+
+### Considerações Adicionais
+
+* O sistema pode ser expandido com funcionalidades adicionais.
+* A interface gráfica pode ser aprimorada com recursos visuais e de acessibilidade.
+* O desempenho do sistema pode ser otimizado com técnicas de balanceamento de carga e otimização de consultas.
+
+### Tecnologias Utilizadas
+
+* Linguagem de programação: C#
+* Interface gráfica: Windows Forms
+* Estrutura de dados: Árvores Binárias de Busca (ABB) e Árvores AVL
+
+### Instalação
+
+1. Clone o repositório.
+2. Abra o projeto no Visual Studio.
+3. Compile e execute o programa.
+
+### Contribuições
+
+Sinta-se à vontade para contribuir com este projeto! Envie suas sugestões, pull requests e issues.
---
+++ Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/Books.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_Final_Estrutura_de_dados_2024._1_Modulo1
+{
+    public partial class Books : UserControl
+    {
+        public Books()
+        {
+            InitializeComponent();
+        }
+
+        private void Books_Load(object sender, EventArgs e)
+        {
+            TransparentBackgroundText();
+        }
+
+        private void TransparentBackgroundText()
+        {
+            lblBookTitle.Parent = btnBooks;
+            lblBookTitle.BackColor = Color.Transparent;
+            lblAuthorName.Parent = btnBooks;
+            lblAuthorName.BackColor = Color.Transparent;
+            lblID.Parent = btnBooks;
+            lblID.BackColor = Color.Transparent;
+            lblPubDate.Parent = btnBooks;
+            lblPubDate.BackColor = Color.Transparent;
+        }
+
+        private int _index;
+        public static int index;
+
+        private void btnBooks_Click(object sender, EventArgs e)
+        {
+            index = _index;
+            Main.openEdit = 1;
+            CadBooks books = new CadBooks();
+            books.Show();
+        }
+
+        private void lblBookTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnBooks.BackColor = Color.Gray;
+        }
+
+        private void lblBookTitle_MouseLeave(object sender, EventArgs e)
+        {
+            btnBooks.BackColor = Color.Gainsboro;
+        }
+
+        private void lblBookTitle_Click(object sender, EventArgs e)
+        {
+            index = _index;
+            Main.openEdit = 1;
+            CadBooks books = new CadBooks();
+            books.Show();
+        }
+
+        private void lblAuthorName_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnBooks.BackColor = Color.Gray;
+        }
+
+        private void lblAuthorName_MouseLeave(object sender, EventArgs e)
+        {
+            btnBooks.BackColor = Color.Gainsboro;
+        }
+
+        private void lblAuthorName_Click(object sender, EventArgs e)
+        {
+            index = _index;
+            Main.openEdit = 1;
+            CadBooks books = new CadBooks();
+            books.Show();
+        }
+
+        private void lblID_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnBooks.BackColor = Color.Gray;
+        }
+
+        private void lblID_MouseLeave(object sender, EventArgs e)
+        {
+            btnBooks.BackColor = Color.Gainsboro;
+        }
+
+        private void lblID_Click(object sender, EventArgs e)
+        {
+            index = _index;
+            Main.openEdit = 1;
+            CadBooks books = new CadBooks();
+            books.Show();
+        }
+
+        private void lblPubDate_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnBooks.BackColor = Color.Gray;
+        }
+
+        private void lblPubDate_MouseLeave(object sender, EventArgs e)
+        {
+            btnBooks.BackColor = Color.Gainsboro;
+        }
+
+        private void lblPubDate_Click(object sender, EventArgs e)
+        {
+            index = _index;
+            Main.openEdit = 1;
+            CadBooks books = new CadBooks();
+            books.Show();
+        }
+
+        public string BookTitle
+        {
+            set { lblBookTitle.Text = value; }
+        }
+
+        public string AuthorName
+        {
+            set { lblAuthorName.Text = value; }
+        }
+
+        public int ID
+        {
+            set { lblID.Text = value.ToString(); }
+        }
+
+        public DateTime PubDate
+        {
+            set { lblPubDate.Text = value.ToString(); }
+        }
+
+        public int Index
+        {
+            set { _index = value; }
+        }
+    }
+}
