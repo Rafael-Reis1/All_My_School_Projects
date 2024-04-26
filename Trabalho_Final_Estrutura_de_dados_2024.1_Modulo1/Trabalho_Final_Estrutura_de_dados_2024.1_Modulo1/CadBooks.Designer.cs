559771f8e2c74f78156faee106121d367799832b
Rafael-Reis <146379316+Rafa3lR@users.noreply.github.com>
2024-04-26 02:42:39 -0300

    Performance update!

---
+++ Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/CadBooks.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Trabalho_Final_Estrutura_de_dados_2024._1_Modulo1
+{
+    partial class CadBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitle = new Label();
+            lblAuthor = new Label();
+            lblPubDate = new Label();
+            tbBookTitle = new TextBox();
+            tbAuthor = new TextBox();
+            dateTimePubDate = new DateTimePicker();
+            btnConfirm = new Button();
+            btnDelete = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = SystemColors.HighlightText;
+            lblTitle.Location = new Point(11, 46);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(75, 21);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Book title";
+            // 
+            // lblAuthor
+            // 
+            lblAuthor.AutoSize = true;
+            lblAuthor.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAuthor.ForeColor = SystemColors.HighlightText;
+            lblAuthor.Location = new Point(11, 111);
+            lblAuthor.Name = "lblAuthor";
+            lblAuthor.Size = new Size(58, 21);
+            lblAuthor.TabIndex = 1;
+            lblAuthor.Text = "Author";
+            // 
+            // lblPubDate
+            // 
+            lblPubDate.AutoSize = true;
+            lblPubDate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPubDate.ForeColor = SystemColors.HighlightText;
+            lblPubDate.Location = new Point(10, 173);
+            lblPubDate.Name = "lblPubDate";
+            lblPubDate.Size = new Size(76, 21);
+            lblPubDate.TabIndex = 2;
+            lblPubDate.Text = "Pub. Date";
+            // 
+            // tbBookTitle
+            // 
+            tbBookTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tbBookTitle.Location = new Point(11, 70);
+            tbBookTitle.Name = "tbBookTitle";
+            tbBookTitle.Size = new Size(243, 25);
+            tbBookTitle.TabIndex = 3;
+            // 
+            // tbAuthor
+            // 
+            tbAuthor.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tbAuthor.Location = new Point(11, 135);
+            tbAuthor.Name = "tbAuthor";
+            tbAuthor.Size = new Size(243, 25);
+            tbAuthor.TabIndex = 4;
+            // 
+            // dateTimePubDate
+            // 
+            dateTimePubDate.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            dateTimePubDate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePubDate.Format = DateTimePickerFormat.Short;
+            dateTimePubDate.Location = new Point(10, 197);
+            dateTimePubDate.Name = "dateTimePubDate";
+            dateTimePubDate.Size = new Size(244, 29);
+            dateTimePubDate.TabIndex = 5;
+            dateTimePubDate.Value = new DateTime(2024, 4, 7, 15, 18, 0, 0);
+            // 
+            // btnConfirm
+            // 
+            btnConfirm.BackColor = Color.LimeGreen;
+            btnConfirm.FlatStyle = FlatStyle.Flat;
+            btnConfirm.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            btnConfirm.Location = new Point(95, 281);
+            btnConfirm.Name = "btnConfirm";
+            btnConfirm.Size = new Size(81, 31);
+            btnConfirm.TabIndex = 6;
+            btnConfirm.Text = "Confirm";
+            btnConfirm.UseVisualStyleBackColor = false;
+            btnConfirm.Click += btnConfirm_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackgroundImage = Properties.Resources.TrashWhite;
+            btnDelete.BackgroundImageLayout = ImageLayout.Stretch;
+            btnDelete.FlatAppearance.BorderSize = 0;
+            btnDelete.FlatAppearance.MouseDownBackColor = Color.Red;
+            btnDelete.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnDelete.FlatStyle = FlatStyle.Flat;
+            btnDelete.Location = new Point(226, 12);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(29, 32);
+            btnDelete.TabIndex = 7;
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // CadBooks
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(267, 340);
+            Controls.Add(btnDelete);
+            Controls.Add(btnConfirm);
+            Controls.Add(dateTimePubDate);
+            Controls.Add(tbAuthor);
+            Controls.Add(tbBookTitle);
+            Controls.Add(lblPubDate);
+            Controls.Add(lblAuthor);
+            Controls.Add(lblTitle);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "CadBooks";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CadBooks";
+            TopMost = true;
+            FormClosing += CadBooks_FormClosing;
+            Load += CadBooks_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblAuthor;
+        private Label lblPubDate;
+        private TextBox tbBookTitle;
+        private TextBox tbAuthor;
+        private DateTimePicker dateTimePubDate;
+        private Button btnConfirm;
+        private Button btnDelete;
+    }
+}
---
+++ Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/Trabalho_Final_Estrutura_de_dados_2024.1_Modulo1/CadBooks.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Trabalho_Final_Estrutura_de_dados_2024._1_Modulo1
+{
+    public partial class CadBooks : Form
+    {
+        public CadBooks()
+        {
+            InitializeComponent();
+        }
+
+        public static string arvore;
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            switch (Main.openEdit)
+            {
+                case 0:
+                    {
+                        PopulatingLists();
+                        int index = Main.id.Count() - 1;
+                        Main.books.Add(new Books());
+                        Main.books[index].ID = Main.id[index];
+                        Main.books[index].BookTitle = Main.title[index];
+                        Main.books[index].AuthorName = Main.author[index];
+                        Main.books[index].PubDate = Main.pubDate[index];
+                        Main.books[index].Index = index;
+                        Main.flowPanelBooks.Controls.Add(Main.books[index]);
+                        Tdata aux = CreatingTreeAuxData();
+                        DateTime time1 = DateTime.Now;
+                        DateTime time2;
+                        TimeSpan timeSpanAvl = InsertingAVLTree(aux, time1);
+                        TimeSpan timeSpanAbb;
+                        InsertingABBTree(out time1, out time2, out timeSpanAbb);
+                        int balanceFactorAbb, balanceFactorAvl;
+                        GettingTreeRootBalanceFactor(out balanceFactorAbb, out balanceFactorAvl);
+                        SavingAndUpdatingDateFilters();
+                        Main.quantBooksCreated++;
+                        ClosingAndShowingTree(timeSpanAvl, timeSpanAbb, balanceFactorAbb, balanceFactorAvl);
+                        break;
+                    }
+
+                default:
+                    UpdatingBook();
+                    break;
+            }
+        }
+
+        private void UpdatingBook()
+        {
+            Main.title[Books.index] = tbBookTitle.Text;
+            Main.author[Books.index] = tbAuthor.Text;
+            Main.pubDate[Books.index] = dateTimePubDate.Value;
+            SaveInTXT.UpdateTXT();
+            Main.openEdit = 0;
+            this.Close();
+        }
+
+        private void ClosingAndShowingTree(TimeSpan timeSpanAvl, TimeSpan timeSpanAbb, int balanceFactorAbb, int balanceFactorAvl)
+        {
+            arvore = "";
+            this.Close();
+            Main.tree.printTree(Main.tree.root, "", true);
+            MessageBox.Show($"FB nó raiz Arv. ABB: {balanceFactorAbb} \n" +
+                $"Tempo de execução da Arv. ABB: {timeSpanAbb.TotalNanoseconds} Nanossegundos\n" +
+                $"FB nó raiz Arv. AVL: {balanceFactorAvl} \n" +
+                $"Numero de rotações Arv. AVL: {Main.numRotation} \n" +
+                $"Tempo de execução da Arv. AVL: {timeSpanAvl.TotalNanoseconds} Nanossegundos\n\n" +
+                arvore,
+                "Arvore de pesquisa",
+                MessageBoxButtons.OK);
+        }
+
+        private static void SavingAndUpdatingDateFilters()
+        {
+            SaveInTXT.UpdateTXT();
+            Main.dateTimePickerMin.Value = Main.pubDate.Min();
+            Main.dateTimePickerMax.Value = Main.pubDate.Max();
+        }
+
+        private static void GettingTreeRootBalanceFactor(out int balanceFactorAbb, out int balanceFactorAvl)
+        {
+            balanceFactorAbb = Main.bst.BalanceFactor();
+            balanceFactorAvl = Main.tree.getRootBalanceFactor();
+        }
+
+        private static void InsertingABBTree(out DateTime time1, out DateTime time2, out TimeSpan timeSpanAbb)
+        {
+            time1 = DateTime.Now;
+            Main.bst.Insert(Main.quantBooksCreated);
+            time2 = DateTime.Now;
+            timeSpanAbb = time2 - time1;
+        }
+
+        private static TimeSpan InsertingAVLTree(Tdata aux, DateTime time1)
+        {
+            Main.tree.root = Main.tree.insert(Main.tree.root, aux);
+            DateTime time2 = DateTime.Now;
+            TimeSpan timeSpanAvl = time2 - time1;
+            return timeSpanAvl;
+        }
+
+        private Tdata CreatingTreeAuxData()
+        {
+            Tdata aux = new Tdata();
+            aux.id = Main.quantBooksCreated;
+            aux.bookTitle = tbBookTitle.Text;
+            aux.author = tbAuthor.Text;
+            aux.pubDate = dateTimePubDate.Value;
+            return aux;
+        }
+
+        private void PopulatingLists()
+        {
+            Main.id.Add(Main.quantBooksCreated);
+            Main.title.Add(tbBookTitle.Text);
+            Main.author.Add(tbAuthor.Text);
+            Main.pubDate.Add(dateTimePubDate.Value);
+        }
+
+        private void CadBooks_Load(object sender, EventArgs e)
+        {
+            dateTimePubDate.Value = DateTime.Today;
+            switch (Main.openEdit)
+            {
+                case 0:
+                    btnDelete.Hide();
+                    break;
+                default:
+                    tbBookTitle.Text = Main.title[Books.index].ToString();
+                    tbAuthor.Text = Main.author[Books.index].ToString();
+                    dateTimePubDate.Value = Main.pubDate[Books.index];
+                    break;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult delete = ConfirmationMessage();
+
+            if (delete == DialogResult.OK)
+            {
+                DateTime time1, time2;
+                TimeSpan timeSpanAvl = DeleteAVLTree();
+                TimeSpan timeSpanAbb;
+                DeleteABBTree(out time1, out time2, out timeSpanAbb);
+                DeleteList();
+                Main.flowPanelBooks.Controls.Remove(Main.books[Books.index]);
+                SavingAndUpdatingDateFilter();
+                ClosingShowingPerformance(timeSpanAvl, timeSpanAbb);
+            }
+        }
+
+        private void ClosingShowingPerformance(TimeSpan timeSpanAvl, TimeSpan timeSpanAbb)
+        {
+            this.Close();
+            MessageBox.Show(
+                $"Tempo de execução da Arv. ABB: {timeSpanAbb.TotalNanoseconds} Nanossegundos\n" +
+                $"Tempo de execução da Arv. AVL: {timeSpanAvl.TotalNanoseconds} Nanossegundos\n\n",
+                "Arvore de pesquisa",
+                MessageBoxButtons.OK);
+        }
+
+        private static void SavingAndUpdatingDateFilter()
+        {
+            SaveInTXT.UpdateTXT();
+            Main.openEdit = 0;
+            Main.dateTimePickerMin.Value = Main.pubDate.Min();
+            Main.dateTimePickerMax.Value = Main.pubDate.Max();
+        }
+
+        private static void DeleteList()
+        {
+            Main.id.RemoveAt(Books.index);
+            Main.title.RemoveAt(Books.index);
+            Main.author.RemoveAt(Books.index);
+            Main.pubDate.RemoveAt(Books.index);
+        }
+
+        private static void DeleteABBTree(out DateTime time1, out DateTime time2, out TimeSpan timeSpanAbb)
+        {
+            time1 = DateTime.Now;
+            Main.bst.Delete(Main.id[Books.index]);
+            time2 = DateTime.Now;
+            timeSpanAbb = time2 - time1;
+        }
+
+        private static TimeSpan DeleteAVLTree()
+        {
+            DateTime time1 = DateTime.Now;
+            Main.tree.root = Main.tree.deleteNode(Main.tree.root, Main.id[Books.index]);
+            DateTime time2 = DateTime.Now;
+            TimeSpan timeSpanAvl = time2 - time1;
+            return timeSpanAvl;
+        }
+
+        private static DialogResult ConfirmationMessage()
+        {
+            return MessageBox.Show("Are you sure?", "Delete Book",
+                            MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+        }
+
+        private void CadBooks_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Main.openEdit = 0;
+        }
+    }
+}
